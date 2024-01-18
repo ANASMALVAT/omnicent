cb81f99a80ce4b5fa1e87b7cf0fd5ab00969b14a
ANASMALVAT <an890779@dal.ca>
2024-01-18 04:55:15 -0400

    landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,190 @@
-import Image from 'next/image'
+"use client"
+
+import MaxWidthWrapper from "@/components/maxWidthWrapper";
+import Link from "next/link";
+import {ArrowRight} from "lucide-react"
+import { buttonVariants } from "@/components/ui/button";
+import Image from "next/image";
+import {motion} from "framer-motion"
+import { useInView } from 'react-intersection-observer';
 
 export default function Home() {
+
+  const [ref, inView] = useInView({
+    triggerOnce: true,
+  });
+
+  const imageVariants = {
+    initial: { x: '-100%' }, // Off-screen to the left
+    animate: { x: 0 } // On-screen
+  };
+  const imageTransition = {
+    duration: 1.5, // Animation duration in seconds
+    ease: "easeInOut" // Easing function, you can use other easing functions as well
+  };
+
+  const sentenceVariants = {
+    initial: {
+      opacity: 0,
+      y: 20,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        delay: 0.1, // Optional delay before the typewriter effect starts
+        type: 'spring', // You can use other transition types like "tween", "spring", etc.
+        stiffness: 100,
+      },
+    },
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <><MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex flex-col items-center justify-center text-center  bg-omniblack">
+
+      <motion.h1 
+        initial="initial"
+        animate="animate"
+        variants={sentenceVariants}
+        className="max-w-4xl text-5xl text-center font-bold md:text-6xl lg:text-7xl text-zinc-200">
+        Talk to your <span className="text-orange-800">PDF</span> like human.
+      </motion.h1>
+
+      <motion.p
+        initial="initial"
+        animate="animate"
+        variants={sentenceVariants}
+        className="mt-10 max-w-prose text-zinc-200 sm:text-lg">
+          Omnicent allows you to talk to your PDF documents. Simply upload your file and start asking questions.
+      </motion.p>
+
+      <Link className={buttonVariants({
+        size:"lg",
+        className:"mt-5 z-10  transition-opacity duration-1000"
+      })}
+       href={""} target="_blank">
+        Get started <ArrowRight className=" ml-2 h-5 w-5"></ArrowRight>
+      </Link>
+    </MaxWidthWrapper>
+
+    <div>
+      <div className=" relative isolate">
+        <div aria-hidden="true" className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80  ">
+          <div 
+          style={{
+            clipPath:"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+          }}
+          className=" relative left-[calc(50%-11rem)] aspect-[1155/678] w-[46.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#f12711] to-[#1fddff] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <motion.div
+        initial="initial"
+        animate="animate"
+        variants={imageVariants}
+        transition={imageTransition}
+        className="mx-auto max-w-6xl px-6 lg:px-8">
+          <div className="mt-16 flow-root sm:mt-24 ">
+            <div className=" -m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg-m-4 lg:rounded-2xl lg:p-4 ">
+              <Image
+                src={"/dashboard-preview.jpg"}
+                alt=""
+                width={1364}
+                height={866}
+                quality={100}
+                className="rounded-md bg-white p-2 sm:p-8 shadow-2xl ring-1 ring-gray-900/10"
+              ></Image>
+            </div>
+          </div>
+        </motion.div>
+        <div>
+          <div className=" relative isolate">
+            <div aria-hidden="true" className="pointer-events-none absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80  ">
+              <div 
+              style={{
+                clipPath:"polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
+              }}
+              className=" relative right-[calc(50%-13rem)] aspect-[1155/678] w-[46.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#1fddff] to-[#f12711] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    <div className=" mx-auto mb-32 mt-21 max-w-5xl sm:mt-56">
+      <div className="mb-12 px-6 lg:px-8">
+        <div className=" mx-auto max-w-2xl sm:text-center">
+            <motion.h2
+            initial="initial"
+            ref={ref}
+            animate={inView ? "animate" : "initial"}
+            variants={sentenceVariants}
+            className="mt-2 font-bold text-4xl text-zinc-200 sm:text-5xl">
+              Talk to PDF in minutes
+            </motion.h2>
+            <motion.p 
+              initial="initial"
+              ref={ref}
+              animate={inView ? "animate" : "initial"}
+              variants={sentenceVariants} className="mt-4 text-lg text-zinc-200">
+              Interact with your PDF like google search with omnicent.
+            </motion.p>
+        </div>
+      </div> 
+      <motion.ol
+      initial="initial"
+      ref={ref}
+      animate={inView ? "animate" : "initial"}
+      variants={sentenceVariants}
+      className="my-8 space-y-4 pt-8 md:flex md:space-x-12 md:space-y-0">
+        <li className=" md:flex-1">
+          <div  className=" flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:bover-t-2 md:pb-0  md:pl-0 md:pt-4"> 
+          <span className=" text-lg font-medium text-blue-300"> Step 1</span>
+          <span className=" text-[15px] font-medium text-zinc-200"> Sign up for an account</span>
+          <span className=" mt-2 text-zinc-300"> Either starting out with a free plan or choose our {' '}</span>
+          <Link href={"/pricing"} className="text-blue-500 underline underline-offset-2"> pro plan</Link>
+          </div>
+        </li>
+        <li className=" md:flex-1">
+          <div  className=" flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:bover-t-2 md:pb-0  md:pl-0 md:pt-4"> 
+          <span className=" text-lg font-medium text-blue-300"> Step 2</span>
+          <span className=" text-[15px] font-medium text-zinc-200"> Upload your PDF file</span>
+          <span className=" mt-2 text-zinc-300"> We&apos;llprocess your file and make it ready for you to interact with.</span>
+          </div>
+        </li>
+        <li className=" md:flex-1">
+          <div  className=" flex flex-col space-y-2 border-l-4 border-zinc-300 py-2 pl-4 md:border-l-0 md:bover-t-2 md:pb-0  md:pl-0 md:pt-4"> 
+          <span className=" text-lg font-medium text-blue-300"> Step 3</span>
+          <span className=" text-[15px] font-medium text-zinc-200"> It&epos; that simple. Try out Omnicent today.</span>
+          <span className=" mt-2 text-zinc-300"> It really takes less than a minute.</span>
+          </div>
+        </li>
+      </motion.ol>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <motion.div
+        initial="initial"
+        animate={inView ? "animate" : "initial"}
+        ref={ref}
+        variants={imageVariants}
+        transition={imageTransition}
+        className="mx-auto max-w-6xl px-6 lg:px-8">
+          <div className="mt-16 flow-root sm:mt-24 ">
+            <div className=" -m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg-m-4 lg:rounded-2xl lg:p-4 ">
+              <Image
+                src={"/file-upload-preview.jpg"}
+                alt=""
+                width={1364}
+                height={866}
+                quality={100}
+                className="rounded-md bg-white p-2 sm:p-8 shadow-2xl ring-1 ring-gray-900/10"
+              ></Image>
+            </div>
+          </div>
+        </motion.div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   )
 }
